34a926c40be310ae9e4cc76b1190468c736cd3ed
Somojit Arambam <somoarambam123@gmail.com>
2026-01-10 15:20:29 +0530

    Progressive web

--- proxy.ts
+++ proxy.ts
@@ -7,6 +7,14 @@ export async function proxy(request: NextRequest) {
 
 export const config = {
     matcher: [
-        '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+        /*
+         * Match all request paths except for the ones starting with:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - manifest.webmanifest (PWA manifest) <--- ADD THIS
+         * - sw.js (Service Worker)          <--- ADD THIS
+         */
+        '/((?!_next/static|_next/image|favicon.ico|manifest.webmanifest|sw.js|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
