5a274783a38902943a384eebe23c37a5cef2cec3
Somojit Arambam <somoarambam123@gmail.com>
2026-01-11 20:59:46 +0530

    Progressive web

--- app/manifest.ts
+++ app/manifest.ts
@@ -3,9 +3,9 @@ import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
     return {
-        name: '',
-        short_name: '',
-        description: '',
+        name: 'Daciana Stationery & Cosmetics',
+        short_name: 'Daciana',
+        description: 'Stationery and Cosmetics Store',
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
@@ -13,16 +13,16 @@ export default function manifest(): MetadataRoute.Manifest {
         icons: [
 
             {
-                src: '/icon-512x512.png',
-                sizes: '512x512',
+                src: '/icon-192x192.png',
+                sizes: '192x192',
                 type: 'image/png',
-                purpose: 'any',
+                purpose: 'maskable',
             },
             {
                 src: '/maskable-icon.png', // Create a new file for this
                 sizes: '512x512',
                 type: 'image/png',
-                purpose: 'maskable',
+                purpose: 'any',
             },
         ],
     }
